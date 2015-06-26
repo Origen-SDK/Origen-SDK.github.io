54efb233309833a57500e10c3b43027e0785c587
Stephen McGinty <stephen.mcginty@freescale.com>
2015-06-26 10:10:13 -0500

    Added custom js and css, removed bootstrap, now loaded from a CDN

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 // Modified from: http://jsfiddle.net/jhfrench/mc4Qd/
+console.log("yo");
 $('.expandcollapse').click(function() {
 
     var newstate = $(this).attr('state') ^ 1,
@@ -6,16 +7,16 @@ $('.expandcollapse').click(function() {
 
     // if state=0, show all the accordion divs within the same block (in this case, within the same section)
     if ( $(this).attr('state')==="0" ) {
-        $(this).parent().find('div.accordion-body:not(.in)').collapse('show');
-        $(this).parent().find('.expandcollapse').html("<i class=\"icon-" + icon + "-sign\"></i>").attr('state', newstate);
+        $(this).parent().find('div.panel-collapse:not(.in)').collapse('show');
+        $(this).parent().find('.expandcollapse').html("<i class=\"fa fa-" + icon + "\"></i>").attr('state', newstate);
     }
     // otherwise, collapse all the divs
     else {
-        $(this).parent().find('div.accordion-body.in').collapse('hide');
-        $(this).parent().find('.expandcollapse').html("<i class=\"icon-" + icon + "-sign\"></i>").attr('state', newstate);
+        $(this).parent().find('div.panel-collapse.in').collapse('hide');
+        $(this).parent().find('.expandcollapse').html("<i class=\"fa fa-" + icon + "\"></i>").attr('state', newstate);
     }
 
-    $(this).html("<i class=\"icon-" + icon + "-sign\"></i>");
+    $(this).html("<i class=\"fa fa-" + icon + "\"></i>");
 
     $(this).attr('state',newstate)
 
