33ec64023a82f0b91b1426d89a254e5172bfebb7
Stephen McGinty <stephen.mcginty@nxp.com>
2018-09-10 16:23:16 +0100

    Added travis push

---
+++ .travis/push.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [ci skip]"
+
+git remote add github https://${GH_TOKEN}@github.com/Origen-SDK/Origen-SDK.github.io
+git push github
