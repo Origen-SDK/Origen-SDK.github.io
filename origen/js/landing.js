9663c12cc9f4e87f166b05de3f23298d4f4ff9cd
Ronnie Lajaunie <ronnie.lajaunie@nxp.com>
2019-03-01 11:44:00 -0700

    Release of origen_core 0.42.2

--- origen/js/landing.js
+++ origen/js/landing.js
@@ -23,7 +23,7 @@ $(function() {
 
     $.ajax({
       type: 'POST',
-      url: 'https://hub.origen-sdk.org/api/subscriptions/origen_news',
+      url: 'http://hub.origen-sdk.org/api/subscriptions/origen_news',
       crossDomain: true,
       data: data,
       dataType: 'json',
